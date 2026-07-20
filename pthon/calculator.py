09de960e3d40ca665dc657476f5adb1742dedd3b
simran02-08-2023 <singhanchal33141@gmail.com>
2026-07-20 23:26:10 +0530

    Initial commit

---
+++ pthon/calculator.py
@@ -0,0 +1,12 @@
+x=int(input("Enter first number "))
+y=int(input("Enter second number "))
+
+sum=x+y
+sub=x-y
+mul=x*y
+div=x/y
+
+print("sum of two numbers is "+str(sum))
+print("subtraction of two numbers is "+str(sub))
+print("multiplication of two numbers is "+str(mul))
+print("division of two numbers is "+str(div))
