e9ad50aa4d07bc623637149f4c998dc155d1baee
simran02-08-2023 <singhanchal33141@gmail.com>
2026-08-15 23:59:24 +0530

    created bucket in aws using python with boto3 library

--- pthon/s3_backup.py
+++ pthon/s3_backup.py
@@ -1,7 +1,17 @@
-"""
-Script to backup files to an AWS s3
- """
-import boto3 # library to interact with AWS services using python
-s3 = boto3.resource("s3") # creating a resource object for s3
+import boto3
+s3 = boto3.resource("s3", region_name="eu-west-1")
 def show_buckets(s3):
-    print(s3.buckets.all())
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+def create_bucket(s3):
+    s3.create_bucket(
+        Bucket="python-for-devops-simran",
+        CreateBucketConfiguration={
+            'LocationConstraint': 'eu-west-1',
+        },
+    )
+    print("Bucket created successfully")
+
+show_buckets(s3)
+create_bucket(s3)
